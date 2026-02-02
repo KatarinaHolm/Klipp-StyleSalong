7eca4f85ef9996ce7fb29934e42998cccd4cdc44
Katarina Holm <katarinaholm89@gmail.com>
2026-02-02 09:16:42 +0100

    Add project files.

---
+++ Data/SalonDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Klipp_StyleSalong.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Klipp_StyleSalong.Data
+{
+    public class SalonDbContext : DbContext
+    {
+
+        public SalonDbContext(DbContextOptions<SalonDbContext> options) : base (options)
+        {
+            
+        }
+        public DbSet<Booking> Bookings { get; set; }
+
+    }
+}
---
+++ Models/Booking.cs
@@ -0,0 +1,17 @@
+﻿namespace Klipp_StyleSalong.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public DateOnly Date{ get; set; }
+
+        public TimeOnly Time { get; set; }
+
+        public string Hairdresser { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string PhoneNr { get; set; }
+    }
+}
